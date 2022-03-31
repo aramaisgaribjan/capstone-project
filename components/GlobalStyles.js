ce3df2eb78848fc169eaf76ec9d98fe6865891fd
Aramais Garibjan <aramo.g@hotmail.de>
2022-03-31 14:54:54 +0200

    change global body

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -4,4 +4,21 @@ export const GlobalStyles = createGlobalStyle`
     *{
         box-sizing: border-box;
     }
+    body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  max-width: 100%;
+  height: 100%;
+  background-color: white;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
 `;
