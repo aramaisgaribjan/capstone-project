91b8d557f1f1d3e6da2baf3f6fe26413c3c0da43
Aramais Garibjan <aramo.g@hotmail.de>
2022-04-20 18:57:59 +0200

    add markers and own location

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import styled from "styled-components";
+
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -30,17 +29,106 @@ export default function Map() {
     libraries,
   });
 
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
   if (loadError) return "Error";
   if (!isLoaded) return "Loading...";
 
+  function Locate({ panTo }) {
+    return (
+      <MyLocation
+        onClick={() => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              panTo({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            },
+            () => null
+          );
+        }}
+      >
+        <img src="/kompass.png" alt="standort" width="70px" />
+      </MyLocation>
+    );
+  }
+
   return (
     <div>
+      <Locate panTo={panTo} />
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={12}
         center={center}
         options={options}
-      ></GoogleMap>
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: "/float.png",
+              scaledSize: new window.google.maps.Size(50, 70),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{
+              lat: selected.lat + 0.0018,
+              lng: selected.lng,
+            }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <P>Erstelle deinen Angeltag</P>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
 }
+
+const P = styled.p`
+  color: black;
+`;
+
+const MyLocation = styled.button`
+  position: absolute;
+  top: 1rem;
+  right: 0;
+  background: none;
+  border: none;
+  z-index: 10;
+`;
