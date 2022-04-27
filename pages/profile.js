4288d55949332287989b810196d984c8818791ba
Aramais Garibjan <aramo.g@hotmail.de>
2022-04-27 11:05:06 +0200

    .

--- pages/profile.js
+++ pages/profile.js
@@ -140,8 +140,8 @@ const AllInfo = styled.div`
   margin-top: 10px;
 `;
 
-export async function getStaticProps(context) {
-  const session = await getSession(context);
+export async function getStaticProps() {
+  const session = await getSession();
   if (!session) {
     return {
       redirect: {
